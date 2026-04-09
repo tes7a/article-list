c91f4038c67edc163ec6b73f1467a2c2ae1e3602
Konstantin <ttes7a@gmail.com>
2026-04-09 17:39:43 +0300

    delete api

--- apps/api/src/server.ts
+++
@@ -1,9 +0,0 @@
-import { app } from './app.js';
-import { getPort } from './config/env.js';
-
-const port = getPort();
-
-app.listen(port, () => {
-  console.log(`API listening on http://localhost:${port}`);
-});
-
